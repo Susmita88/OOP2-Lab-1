6660464558337ef5bea356447af3df1aacc4a568
Shushmita Chowdhury <anamica.chowdhury31@gmail.com>
2020-10-31 12:16:02 +0600

    Lab Task 3 added

---
+++ Lab Task 3/Lab Task 3/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_3
+{
+    class Student
+    {
+        string name;
+        string id;
+        string department;
+        float cgpa;
+
+        public void insert(string Name, string Id, string Department, float cgpa)
+        {
+            name = Name;
+            id = Id;
+            department = Department;
+            cgpa = CGPA;
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Id: " + id);
+            Console.WriteLine("Department: " + department);
+            Console.WriteLine("CGPA: " + cgpa);
+
+        }
+    }
+}
