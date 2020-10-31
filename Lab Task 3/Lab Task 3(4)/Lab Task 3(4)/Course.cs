edd64e02eda823074157d02e14ba0519d8f9c5b4
Shushmita Chowdhury <anamica.chowdhury31@gmail.com>
2020-10-31 13:59:54 +0600

    Lab Task 3(3) added

---
+++ Lab Task 3/Lab Task 3(4)/Lab Task 3(4)/Course.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_3_4_
+{
+    class Course
+    {
+        string courseName;
+        string courseCode;
+        int courseCredit;
+        public string getCourseName()
+        {
+            return courseName;
+        }
+        public void setCourseName(string CourseName)
+        {
+            courseName = CourseName;
+        }
+        public string getCourseCode()
+        {
+            return courseCode;
+        }
+        public void setCourseCode(string CourseCode)
+        {
+            courseCode = CourseCode;
+        }
+        public int getCourseCredit()
+        {
+            return courseCredit;
+        }
+        public void setCourseCredit(int CourseCredit)
+        {
+            courseCredit = CourseCredit;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + courseName);
+            Console.WriteLine("Code: " + courseCode); 
+            Console.WriteLine("Credit: " + courseCredit);
+        }
+    }
+}
