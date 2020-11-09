567433a2f69c89278a4823d5e6d98f4f9b8a8e0a
Shushmita Chowdhury <anamica.chowdhury31@gmail.com>
2020-11-09 13:48:56 +0600

    Lab Task 6 added

---
+++ Lab Task 6/Lab Task 6/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_6
+{
+    class Account
+    {
+        
+        private string accName;
+        private string acid;
+        private int balance;
+
+        public string getAccName()
+            {
+                return accName;
+            }
+        public void setAccName(string AccName)
+            {
+                accName = AccName;
+            }
+
+        public string getAcid()
+        {
+            return acid;
+        }
+        public void setAcid(string Acid)
+        {
+            acid = Acid;
+        }
+
+        public int getBalance()
+        {
+            return balance;
+        }
+        public void setBalance(int Balance)
+        {
+            balance = Balance;
+        }
+        public Account()
+        {
+            Console.WriteLine("Empty Constructor");
+        }
+        public Account(string accName, string acid, int balance)
+        {
+            this.accName = accName;
+            this.acid = acid;
+            this.balance = balance;
+
+        }
+        public void Deposit(int amount)
+        {
+            balance = balance + amount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            balance = balance - amount;    
+        }
+
+        public void Transfer(int amount, Account receiver)
+        {
+            receiver.balance = balance + amount;
+        }
+       
+    }
+}
---
+++ Lab Task 6/Lab Task 6/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_6
+{
+    class Student
+    {
+       
+       private string name;
+       private string id;
+       private string department;
+       private float cgpa;
+
+        public string getName()
+        {
+            return name;
+        }
+        public void setName(string Name)
+        {
+            name = Name;
+        }
+
+        public string getId()
+        {
+            return id;
+        }
+        public void setId(string Id)
+        {
+            id = Id;
+        }
+
+        public string getDepartment()
+        {
+            return department;
+        }
+        public void setDepartment(string Department)
+        {
+            department = Department;
+        }
+        public float getCGPA()
+        {
+            return cgpa;
+        }
+        public void setCGPA(float CGPA)
+        {
+            cgpa = CGPA;
+        }
+        public Student()
+        {
+            Console.WriteLine("Empty Constructor");
+
+        }
+        public Student(string name, string id, string department, float cgpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.department = department;
+            this.cgpa = cgpa;
+            
+        }
+       
+            void ShowInfo()
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Id: " + id);
+                Console.WriteLine("Department: " + department);
+                Console.WriteLine("CGPA: " + cgpa);
+            }
+        }
+}
