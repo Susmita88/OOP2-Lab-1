4a455d73796af01e7bd8c8e540656f99ca9d1e24
Shushmita Chowdhury <anamica.chowdhury31@gmail.com>
2020-11-20 01:06:20 +0600

    Lab Task 3 added

---
+++ Lab Task 3/Lab 3/Lab 3/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Triangle
+    {
+        int x;
+        int y;
+        int z;
+        public int getX()
+        {
+            return x;
+
+        }
+        public void setX(int X)
+        {
+            x = X;
+        }
+
+        public int getY()
+        {
+            return y;
+
+        }
+        public void setY(int Y)
+        {
+            y = Y;
+        }
+        public int getZ()
+        {
+            return z;
+
+        }
+        public void setZ(int Z)
+        {
+            z = Z;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("X: " + x);
+            Console.WriteLine("Y: " + y);
+            Console.WriteLine("Z: " + z);
+        }
+        public void TestTriangle()
+        {
+            if (x == y && y == z)
+            {
+                Console.WriteLine("The triangle is equilateral");
+            }
+            else if (x == y || y == z || x == z)
+            {
+                Console.WriteLine("The triangle is isosceles");
+            }
+            else
+            {
+                Console.WriteLine("The triangle is Scaleme");
+            }
+        }
+    }
+
+}
